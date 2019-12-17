9f92ad5a29fb3b6f75d6c3b9986ca56feb2fc7f9
coliner300 <54290215+coliner300@users.noreply.github.com>
2019-12-17 14:13:22 -0800

    done

---
+++ story1.java
@@ -0,0 +1,62 @@
+public class story1
+{
+    private static String story1 = "Welcome to the zombie apocalypse. " +
+            "You have lost your entire family due to running from the unexpected epidemic of the zombie apocalypse." +
+            " You are now going to try to get to the survivor outpost in one piece. Enter 1 to continue, enter 2 to exit.";
+    private static String message1 = "The lesson of this story is that you have to eat to survive.";
+    private static String message2 = "The lesson of this story is that you cannot outrun the zombies no matter what you do.";
+    private static String message3 = "The lesson of this story is that if you get spotted by zombies, then your best choice os to fight.";
+    private static String message4 = "The lesson of this story is that you have to make peace with humans, because no one can trust anyone at this time.";
+    private static String message5 = "The lesson of this story is that you shouldn't use a pew pew during the apocalypse because it is unreliable.";
+
+
+    public story1()
+    {
+        this.story1 = story1;
+    }
+    public void message1()
+    {
+        this.message1 = message1;
+    }//end message1
+    public void message2()
+    {
+        this.message2 = message2;
+    }//end message3
+    public void message3()
+    {
+        this.message3 = message3;
+    }//end message3
+    public void message4()
+    {
+        this.message4 = message4;
+    }//end message4
+    public void message5()
+    {
+        this.message5 = message5;
+    }//end message5
+
+    public static String getStory1()
+    {
+        return story1;
+    }//complete
+    public static String getMessage1()
+    {
+        return message1;
+    }//end getMessage1
+    public static String getMessage2()
+    {
+        return message2;
+    }//end getmessage2
+    public static String getMessage3()
+    {
+        return message3;
+    }//end getmessage3
+    public static String getMessage4()
+    {
+        return message4;
+    }//end getmessage4
+    public static String getMessage5()
+    {
+        return message5;
+    }//end getmessage5
+}
