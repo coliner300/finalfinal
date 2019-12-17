f80538d6910d0b81db70ba3188c468d2fe19deef
coliner300 <54290215+coliner300@users.noreply.github.com>
2019-12-17 14:13:41 -0800

    done

---
+++ story2.java
@@ -0,0 +1,119 @@
+public class story2
+{
+    private static String story2 = ("It is the middle of the night and you have come to a building that looks safe to enter, " +
+            "do you enter the building(1) or continue through the night(2)?");
+    private static String story2b = ("You have chosen to stay the night in a hotel, you find a room that is empty and the mini " +
+            "fridge inside has drinks and food in it, will you eat and drink from the mini fridge?");
+    private static String story2c =("You chose to eat and drink the food in the mini fridge. You don't feel good so you go to sleep. " +
+            "You wake up and you are feeling better, do you start your journey again(1) or stay inside for a little while longer(2).");
+    private static String story2d = ("You leave on your journey in the morning and travel for many days. You forget that you are a human " +
+            "and have to eat.");//ending
+    private static String story2e = ("You begin on your journey for 4 days and you can see the survivor outpost. Do you make a peace banner(1) " +
+            "or run at them as fast as you can(2).");
+    private static String story2f = ("You stay in the building a little longer. When you leave the building their is a horde outside. Do you run " +
+            "past them(1) or go back inside(2)");
+    private static String story2g = ("You run past the zombies but they are to fast. They catch you and you die.");
+    private static String story2h = ("You try to go back in the building to hide, but the zombies spotted you and they are persistent. They follow you back to your room and you can't escape.");
+    private static String story2i = ("You make a peace banner, and you travel towards the outpost. You are protected from the zombies while you make a run for the outpost. " +
+            "You get there alive and you live happily ever after.");
+    private static String story2j = ("You run at the outpost as fast as you can, but they see you and they don't know why you are running at them and they don't protect you. You don't make it to" +
+            "the camp and you survive.");
+
+    public story2()
+    {
+        this.story2 = story2;
+    }//end story2
+
+    public void story2b()
+    {
+        this.story2b = story2b;
+    }//end story2b
+
+    public void story2c()
+    {
+        this.story2c = story2c;
+    }//end story2c
+
+    public void story2d()
+    {
+        this.story2d = story2d;
+    }//end story2d
+
+    public void story2e()
+    {
+        this.story2e = story2e;
+    }//end story2e
+
+    public void story2f()
+    {
+        this.story2f = story2f;
+    }//end void story2f
+
+    public void story2g()
+    {
+        this.story2g = story2g;
+    }//end void story2g
+
+    public void story2h()
+    {
+        this.story2h = story2h;
+    }//end void story2h
+
+    public void story2i()
+    {
+        this.story2i = story2i;
+    }//end void story2i
+
+    public void story2j()
+    {
+        this.story2j = story2j;
+    }//end void story2j
+
+    public static String getStory2()//complete
+    {
+        return story2;
+    }//end getStory2
+
+    public static String getStory2b()//complete
+    {
+        return story2b;
+    }//end getStory2b
+
+    public static String getStory2c()//complete
+    {
+        return story2c;
+    }//end getStory2c
+
+    public static String getStory2d()//complete
+    {
+        return story2d;
+    }//end getStory2d
+
+    public static String getStory2e()//complete
+    {
+        return story2e;
+    }//end getStory2e
+
+    public static String getStory2f()//complete
+    {
+        return story2f;
+    }//end getStory2f
+
+    public static String getStory2g()//complete
+    {
+        return story2g;
+    }//end getStory2g
+    public static String getStory2h()//complete
+    {
+        return story2h;
+    }//end getStory2h
+    public static String getStory2i()//complete
+    {
+        return story2i;
+    }//end getStory2i
+    public static String getStory2j()//complete
+    {
+        return story2j;
+    }//end getStory2j
+
+}
