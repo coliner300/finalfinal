cbae1adf5fb3389df5526a342bdb0bdcb904552f
coliner300 <54290215+coliner300@users.noreply.github.com>
2019-12-17 14:13:55 -0800

    Create story3.java

---
+++ story3.java
@@ -0,0 +1,77 @@
+public class story3
+{
+    private static String story3 = ("You carry on traveling through the night and avoid zombies, " +
+            "you then come upon travellers, do you join them in their travels(1) or avoid them and " +
+            "stay alone on your journey(2)?");
+    private static String story3a = ("You join the travellers, but when you walk up to them they are zombies. Do you fight them off(1), or run away(2)?");
+    private static String story3b = ("You have to fight off the zombies, do you use the crowbar(1) or a pew pew(2)?");
+    private static String story3c = ("You start to run away from the zombies, but you run out of stamina and they catch you.");//end of story
+    private static String story3d = ("You chose to fight them off with the pew pew. You run out of pew pews and they eat you.");//end of story
+    private static String story3e = ("You use a crowbar to fight off the zombies, but they overpower you and they eat you.");//end of story
+    private static String story3f = ("You stay alone on your journey, but it is night so you get surrounded by zombies.");//end of story
+
+    public void story3()
+    {
+        this.story3 = story3;
+    }//end story3
+
+    public void story3a()
+    {
+        this.story3a = story3a;
+    }//end story3a
+
+    public void story3b()
+    {
+        this.story3b = story3b;
+    }//end story3b
+
+    public void story3c()
+    {
+        this.story3c = story3c;
+    }//end story3c
+
+    public void story3d()
+    {
+        this.story3d = story3d;
+    }//end story3d
+
+    public void story3e()
+    {
+        this.story3e = story3e;
+    }//end story3e
+
+    public void story3f()
+    {
+        this.story3f = story3f;
+    }//end story3f
+
+
+    public static String getStory3()
+    {
+        return story3;
+    }//complete
+    public static String getStory3a()
+    {
+        return story3a;
+    }//complete
+    public static String getStory3b()
+    {
+        return story3b;
+    }//complete
+    public static String getStory3c()
+    {
+        return story3c;
+    }//complete
+    public static String getStory3d()
+    {
+        return story3d;
+    }//complete
+    public static String getStory3e()
+    {
+        return story3e;
+    }//complete
+    public static String getStory3f()
+    {
+        return story3f;
+    }//complete
+}
